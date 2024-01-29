e8bb7da50824fb1039e7dc9f424c6565aa4bc494
Indra090800 <indramaulana.9c@gmail.com>
2024-01-29 14:31:52 +0700

    b

--- resources/views/pesanan/myrate.blade.php
+++ resources/views/pesanan/myrate.blade.php
@@ -98,11 +98,12 @@
                                         <form action="/rating/{{ $k->rating_id }}/deleteS" method="POST" style="margin-left: 5px;">
                                             @csrf
                                             <a class="btn btn-danger btn-sm btnEdit">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eraser" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                            <path d="M19 20h-10.5l-4.21 -4.3a1 1 0 0 1 0 -1.41l10 -10a1 1 0 0 1 1.41 0l5 5a1 1 0 0 1 0 1.41l-9.2 9.3"></path>
-                                            <path d="M18 13.3l-6.3 -6.3"></path>
-                                            </svg>
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eraser" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <path d="M19 20h-10.5l-4.21 -4.3a1 1 0 0 1 0 -1.41l10 -10a1 1 0 0 1 1.41 0l5 5a1 1 0 0 1 0 1.41l-9.2 9.3"></path>
+                                                <path d="M18 13.3l-6.3 -6.3"></path>
+                                                </svg>
+                                            </a>
                                         </form>
                                         </div>
                                     </td>
